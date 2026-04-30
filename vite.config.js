bf05ef0fa42d8151d26d05c48f1e0a5cd22e57a3
xiaolong027 <xiaolong027gg@gmail.com>
2026-04-30 21:48:30 +0800

    再次完善pwa设置

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/',
+  base: '/meowScan/',
   plugins: [
     vue(),
     tailwindcss(),
